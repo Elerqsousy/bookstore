224870aaf61791c679b00b7721ca9b3ede9603bf
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-05-14 22:06:31 +0200

    Add app styling and missing data skeleton

--- src/components/Books.js
+++ src/components/Books.js
@@ -13,16 +13,18 @@ const Books = () => {
   }, []);
 
   return (
-    <div>
-      {bookList.map((book) => (
-        <BookCard
-          key={book.item_id}
-          id={book.item_id}
-          category={book.category}
-          title={book.title}
-          author={book.author}
-        />
-      ))}
+    <div className="books-container">
+      <section className="book-cards-container">
+        {bookList.map((book) => (
+          <BookCard
+            key={book.item_id}
+            id={book.item_id}
+            category={book.category}
+            title={book.title}
+            author={book.author}
+          />
+        ))}
+      </section>
       <AddBookForm />
     </div>
   );
